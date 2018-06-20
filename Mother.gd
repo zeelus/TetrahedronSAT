a62d6851861e1b455ee91bbff579f857d8ede5f5
Gilbert Gwizdała <gilbert.gwizdala@alior.pl>
2018-06-20 20:01:53 +0200

    Add os normalization.

--- Mother.gd
+++ Mother.gd
@@ -25,26 +25,27 @@ func _physics_process(delta):
 			var nodeBVertex = []
 			
 			for mVertex in self.modelVertex:
-				nodeAVertex.append(nodeA.rotation_representation.xform(mVertex))
-				nodeBVertex.append(nodeB.rotation_representation.xform(mVertex))
+				nodeAVertex.append(nodeA.rotation_representation.xform(mVertex) + nodeA.position)
+				nodeBVertex.append(nodeB.rotation_representation.xform(mVertex) + nodeB.position)
 			
 			for vector in self.vectorP:
-				analizeOS.append(nodeA.rotation_representation.xform(vector))
-				analizeOS.append(nodeB.rotation_representation.xform(vector))
+				analizeOS.append(nodeA.rotation_representation.xform(vector).normalized())
+				analizeOS.append(nodeB.rotation_representation.xform(vector).normalized())
 			
 			for vectorA in self.vectorK:
 				var nodeAVector = nodeA.rotation_representation.xform(vectorA)
 				for vectorB in self.vectorK:
 					var nodeBVector = nodeA.rotation_representation.xform(vectorB)
-					var cross = nodeAVector.cross(nodeBVector)
+					var cross = nodeAVector.cross(nodeBVector).normalized()
 					analizeOS.append(cross)
-					
+			
+			print(len(analizeOS))
 			
 			for os in analizeOS:
 				if self.supportFunc(nodeAVertex, nodeBVertex, os):
+					print("r")
 					return
-				else:
-					print("Dotniecie")
+			print("ASd")
  
 func _process(delta):
 	if time > 3:
@@ -54,26 +55,22 @@ func _process(delta):
 # zwaraca vektory scian w układzie lokalnym
 func getVectorP():
 	var vectores = []
-#	vectores.append(modelVertex[0].cross(modelVertex[1]))
-#	vectores.append(modelVertex[0].cross(modelVertex[3]))
-#	vectores.append(modelVertex[1].cross(modelVertex[3]))
-#	vectores.append(modelVertex[2].cross(modelVertex[3]))
-	vectores.append( (modelVertex[1] - modelVertex[0]).cross((modelVertex[2] - modelVertex[0])) )
-	vectores.append( (modelVertex[1] - modelVertex[0]).cross((modelVertex[3] - modelVertex[0])) )
-	vectores.append( (modelVertex[2] - modelVertex[1]).cross((modelVertex[3] - modelVertex[1])) )
-	vectores.append( (modelVertex[0] - modelVertex[2]).cross((modelVertex[3] - modelVertex[2])) )	
+	vectores.append( (modelVertex[1] - modelVertex[0]).cross((modelVertex[2] - modelVertex[0])).normalized() )
+	vectores.append( (modelVertex[1] - modelVertex[0]).cross((modelVertex[3] - modelVertex[0])).normalized() )
+	vectores.append( (modelVertex[2] - modelVertex[1]).cross((modelVertex[3] - modelVertex[1])).normalized() )
+	vectores.append( (modelVertex[0] - modelVertex[2]).cross((modelVertex[3] - modelVertex[2])).normalized() )	
 	return vectores
 	
 # zwaraca vektory krawędzi w układzie lokalnym
 func getVectorK():
 	var vectores = []
-	vectores.append(modelVertex[0] - modelVertex[1])
-	vectores.append(modelVertex[1] - modelVertex[2])
-	vectores.append(modelVertex[2] - modelVertex[0])
+	vectores.append((modelVertex[0] - modelVertex[1]).normalized() )
+	vectores.append((modelVertex[1] - modelVertex[2]).normalized() )
+	vectores.append((modelVertex[2] - modelVertex[0]).normalized() )
 	
-	vectores.append(modelVertex[3] - modelVertex[0])
-	vectores.append(modelVertex[3] - modelVertex[1])
-	vectores.append(modelVertex[3] - modelVertex[2]) 
+	vectores.append((modelVertex[3] - modelVertex[0]).normalized() )
+	vectores.append((modelVertex[3] - modelVertex[1]).normalized() )
+	vectores.append((modelVertex[3] - modelVertex[2]).normalized() ) 
 	return vectores
 # wykrycie przekrycia
 
@@ -82,32 +79,24 @@ func supportFunc(nodeAVertex, nodeBVertex, os):
 	var maxA = -1000
 	for nodeA in nodeAVertex:
 		var val = nodeA.dot(os)
-		if val < minA:
+		if val <= minA:
 			minA = val
-		if val > maxA:
+		if val >= maxA:
 			maxA = val
 			
 	var minB = 1000
 	var maxB = -1000
 	for nodeB in nodeBVertex:
 		var val = nodeB.dot(os)
-		if val < minB:
+		if val <= minB:
 			minB = val
-		if val > maxB:
+		if val >= maxB:
 			maxB = val
 
-	return (minB >= minA && minB <= maxA) || (maxB >= minA && maxB <= maxA)
-	
-
- 
-#func supportFunc(boxA, boxB, t):
-#	var boxAMinVal = boxA.position.dot(t) - abs(boxA.get_local_direction('x').dot(t)) - abs(boxA.get_local_direction('y').dot(t)) - abs(boxA.get_local_direction('z').dot(t))
-#	var boxAMaxVal = boxA.position.dot(t) + abs(boxA.get_local_direction('x').dot(t)) + abs(boxA.get_local_direction('y').dot(t)) + abs(boxA.get_local_direction('z').dot(t))
- 
-#	var boxBMinVal = boxB.position.dot(t) - abs(boxB.get_local_direction('x').dot(t)) - abs(boxB.get_local_direction('y').dot(t)) - abs(boxB.get_local_direction('z').dot(t))
-#	var boxBMaxVal = boxB.position.dot(t) + abs(boxB.get_local_direction('x').dot(t)) + abs(boxB.get_local_direction('y').dot(t)) + abs(boxB.get_local_direction('z').dot(t))
- 
-#	if boxAMinVal < boxBMinVal:
-#		return boxAMaxVal < boxBMinVal
-#	else:
-#		return boxBMaxVal < boxAMinVal
+	var valX = true
+	if minA < minB:
+		valX = maxA < minB
+	else:
+		valX = maxB < minA
+		
+	return valX
